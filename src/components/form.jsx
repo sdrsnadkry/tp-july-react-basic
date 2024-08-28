7b1edf368d2238857ec7aab1f843ea2e8d3290dc
Sudarshan Adhikari <sdadkry95@gmail.com>
2024-08-28 14:00:11 +0545

    phone validation

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,25 +1,43 @@
 import React, { useState } from "react";
 
+const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const phoneNumber = "9840012345";
+
+// [9,8,4,0,0,1,2,3,4,5,a,c]
+const pArr = phoneNumber.split("");
+
+//check if every element of pArr is inside number array
+const isValidNumber= pArr.every((element) => {
+  return number.includes(Number(element))
+})
+
+
+console.log(isValidNumber);
+
 const Form = () => {
   const [values, setValues] = useState({
     name: "",
     email: "",
     address: "",
+    phone: "",
   });
 
   const [errorObj, setErrorObj] = useState({});
 
   const handleSubmit = () => {
-    const { name, email, address } = values;
+    const { name, email, address, phone } = values;
 
     const emailContainsSymbol = email.includes("@");
     const emailContainsDomain = email.includes(".com");
+    // const isValidPhoneNumber = phone.length === 10 && ;
 
-    if (!name || !address || !email) {
+    if (!name || !address || !email || !phone) {
       return setErrorObj({
         name: !name ? "Name is required" : "",
         email: !email ? "Email is required" : "",
         address: !address ? "Address is required" : "",
+        phone: !phone ? "Phone no is required" : "",
       });
     }
 
@@ -99,6 +117,24 @@ const Form = () => {
           />
           {errorObj.address && (
             <span style={{ color: "red" }}>{errorObj.address}</span>
+          )}
+        </div>
+        <div>
+          <label htmlFor="phone">Phone</label>
+          <input
+            type="text"
+            id="phone"
+            name="phone"
+            value={values.phone}
+            onChange={(event) => {
+              setValues({
+                ...values,
+                phone: event.target.value,
+              });
+            }}
+          />
+          {errorObj.phone && (
+            <span style={{ color: "red" }}>{errorObj.phone}</span>
           )}
         </div>
 
